3dac0fdf7a248b353164aac38e0eb46fbd7b8b67
UnworthyPlebs <127642946+UnworthyPlebs@users.noreply.github.com>
2024-03-22 21:02:03 -0400

    Update data.py

--- src/data.py
+++ src/data.py
@@ -7,7 +7,7 @@ from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.dataloader import default_collate
 from torch.utils.data.distributed import DistributedSampler
-
+os.environ['RANK'] = '0'
 dist.init_process_group(backend='nccl')
 data_stats = {'MNIST': ((0.1307,), (0.3081,)), 'FashionMNIST': ((0.2860,), (0.3530,)),
               'CIFAR10': ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
